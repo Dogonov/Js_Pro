567f9afba9606cf3b02c8db2ccc83f4e80fb0c09
Dogonov <dogonov1996@gmail.com>
2022-08-26 14:59:13 +0300

    Hw4

---
+++ Js_pro_hw8/1_json.js
@@ -0,0 +1,23 @@
+
+/*
+  Завдання:
+   Написати скрипт який:
+     1. Збирає дані з форми (3 різних полів), конвертує їх у json та виводить у консоль.
+   -> 2. Зробити окремий інпут, який виконує JSON.parse(); на той рядок, що ви туди ввели і виводить результат у консоль.
+
+  Array.from(HTMLNodeColection); -> Arary
+
+  <form>
+    <input name="name" />
+    <input name="age"/>
+    <input name="password"/>
+    <button></button>
+  </form>
+
+  <form>
+    <input />
+    <button></button>
+  </form>
+  -> '{"name" : !"23123", "age": 15, "password": "*****" }'
+
+*/
---
+++ Js_pro_hw8/3_async.js
@@ -0,0 +1,18 @@
+/*
+
+Завдання:
+
+     Написати за допомогою async-await скрипт, який:
+
+     Отримує список користувачів: https://jsonplaceholder.typicode.com/users
+     Перебирає, виводить табличку:
+
+     # | userName | email | Показати webiste | Показати телефон |
+     1. | userName | email@test.com | button | button
+     2. | userName | email@test.com | 20/10/2019 | button
+     3. | userName | email@test.com | button | button
+     4. | userName | email@test.com | button | button
+
+     Дані про сайт та номер телефону приховувати під час виведення та показувати під час кліку.
+
+*/
