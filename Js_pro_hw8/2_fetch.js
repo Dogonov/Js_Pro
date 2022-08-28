cae26473544519222947e6ae5f7c4c43ec33f30d
Dogonov <dogonov1996@gmail.com>
2022-08-28 19:08:57 +0300

    HW 8

--- Js_pro_hw8/2_fetch.js
+++ Js_pro_hw8/2_fetch.js
@@ -41,12 +41,29 @@
 */
 
 
-  // fetch(url)
-  //   .then(testFunc)
-  //   .then(test2Func)
-  //   .then( res => {
-  //      return fetch()
-  //       .then( friendsResponse)
-  //       .then()
-  //   })
-  //   .then( func)
+// fetch(url)
+//   .then(testFunc)
+//   .then(test2Func)
+//   .then( res => {
+//      return fetch()
+//       .then( friendsResponse)
+//       .then()
+//   })
+//   .then( func)
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(testFunc => testFunc.json())
+  .then(testFunc => {
+    return testFunc[Math.floor(Math.random() * testFunc.length)];
+  })
+  .then(testFunc => {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(test2Func => test2Func.json())
+      .then(test2Func => {
+        let obj = {};
+        obj.name = testFunc.name;
+        obj.posts = test2Func.filter(x => x.userId === testFunc.id);
+
+        console.log(obj);
+      })
+  });
--- Js_pro_hw8/3_async.js
+++ Js_pro_hw8/3_async.js
@@ -16,3 +16,75 @@
      Дані про сайт та номер телефону приховувати під час виведення та показувати під час кліку.
 
 */
+async function getData() {
+     var result = await fetch('https://jsonplaceholder.typicode.com/users');
+     var data = await result.json();
+     return data;
+}
+
+async function foundTable() {
+     const data = await getData();
+     new Table(document.querySelector('body'), data);
+}
+
+foundTable();
+
+class Table {
+     constructor(el, data) {
+          this.el = el;
+          this.data = data;
+          this.Build();
+     }
+
+     Build() {
+          this.el.appendChild(this.BuildHeader());
+          this.el.appendChild(this.BuildBody());
+     }
+
+     BuildHeader() {
+          var tableHead = document.createElement('thead');
+          tableHead.innerHTML = `
+             <tr>
+                 <th>#</th>
+                 <th>user name</th>
+                 <th>email</th>
+                 <th>Показать webiste</th>
+                 <th>Показать phone</th>
+             </tr>
+         `;
+          return tableHead;
+     }
+
+     BuildBody() {
+          var tableBody = document.createElement('tbody');
+
+          this.data.forEach((item, i) => {
+               var bodyRow = document.createElement('tr');
+               bodyRow.innerHTML = `
+                 <td>${i}</td>
+                 <td>${item.username}</td>
+                 <td>${item.email}</td>
+                 <td>
+                     <button class="_btnWebsite">show</button>
+                     <span class="hide">${item.website}</span>
+                 </td>
+                 <td>
+                     <button class="_btnEmail">show</button>
+                     <span class="hide">${item.phone}</span>
+                 </td>
+             `;
+
+               bodyRow.querySelector('._btnWebsite').addEventListener('click', (e) => {
+                    e.target.classList.toggle('show');
+               });
+
+               bodyRow.querySelector('._btnEmail').addEventListener('click', (e) => {
+                    e.target.classList.toggle('show');
+               });
+
+               tableBody.appendChild(bodyRow);
+          });
+
+          return tableBody;
+     }
+}        
